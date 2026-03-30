2c8fcb08fb6fd30c089d7b9e4fc8032e38e65460
Steiner <dagger@pixelated.love>
2026-03-30 09:33:37 -0400

    🧪 QA: Add test for SubtitleProcessor clean_vtt edge case (#101)
    Merged by PR Churner Bot after addressing review comments

---
+++ tests/utils/test_subtitle_processor.py
@@ -0,0 +1,16 @@
+import pytest
+from utils.subtitle_processor import SubtitleProcessor
+
+def test_clean_vtt_removes_metadata_and_timestamps():
+    vtt_content = """WEBVTT
+Kind: captions
+Language: en
+
+00:00:00.000 --> 00:00:02.500 align:start position:0%
+<c.colorE5E5E5>Hello</c>
+
+00:00:02.500 --> 00:00:05.000 align:start position:0%
+<c.colorE5E5E5>Hello world</c>
+"""
+    result = SubtitleProcessor.clean_vtt(vtt_content)
+    assert result == "Hello Hello world"
